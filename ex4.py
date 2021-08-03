66d2b06034bac6ee2dffb560d6b8b530ef088ccf
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-08-03 12:37:30 +0200

    Enter studydrill comments

---
+++ ex4.py
@@ -0,0 +1,25 @@
+# Exercise 4: Variables and Names
+
+# numbers of cars
+cars = 100
+# how many peopl can go into a car
+space_in_a_car = 4.0
+# number of available driver
+drivers = 30
+# passengers we need to transport
+passengers = 90
+# number of cars that will be empty because of no driver
+cars_not_driven = cars - drivers
+# number of cars that have a driver
+cars_driven = drivers
+# number of people that can be transported
+carpool_capacity = cars_driven * space_in_a_car
+# average number of passengers in each car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("there are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
