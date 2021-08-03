084cd8a31804598cd5b1ab416660f87cb730be7f
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-08-03 12:23:11 +0200

    Exercise 3 Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,39 @@
+# Chapter 3: Numbers and Math
+
+# print a string
+print("I will now count my chickens:")
+
+# print a string and the result of the calculation
+print("Hens", 25 + 30 / 6)
+# print a string and the result of the calculation
+print("Roosters", 100 - 25 * 3 % 4)
+
+# print a string
+print("Now I will count the eggs:")
+
+# print the result of the calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# print a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# print the result of the comparison
+print(3 + 2 < 5 - 7)
+
+# print a string and the result of the calculation
+print("What is 3 + 2: ", 3 + 2)
+# print a string and the result of the calculation
+print("What is 5 - 7: ", 5 - 7)
+
+# print a string
+print("Oh, that's why it's False.")
+
+# print a string
+print("How about some more.")
+
+# print a string and the result of the comparison
+print("Is it greater?", 5 > -2)
+# print a string and the result of the comparison
+print("Is it greater or equal?", 5 >= -2)
+# print a string and the result of the comparison
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3_floating_point.py
@@ -0,0 +1,39 @@
+# Exercise 3: Numbers and Math
+
+# print a string
+print("I will now count my chickens:")
+
+# print a string and the result of the calculation
+print("Hens", 25.0 + 30.0 / 6.0)
+# print a string and the result of the calculation
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# print a string
+print("Now I will count the eggs:")
+
+# print the result of the calculation
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# print a string
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# print the result of the comparison
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# print a string and the result of the calculation
+print("What is 3.0 + 2.0: ", 3.0 + 2.0)
+# print a string and the result of the calculation
+print("What is 5.0 - 7.0: ", 5.0 - 7.0)
+
+# print a string
+print("Oh, that's why it's False.")
+
+# print a string
+print("How about some more.")
+
+# print a string and the result of the comparison
+print("Is it greater?", 5.0 > -2.0)
+# print a string and the result of the comparison
+print("Is it greater or equal?", 5.0 >= -2.0)
+# print a string and the result of the comparison
+print("Is it less or equal?", 5.0 <= -2.0)
