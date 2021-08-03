de172d0c61aebf39f624cc7c35d45603151ee99a
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-08-03 17:15:01 +0200

    Add exercise Printing, Printing, Printing

---
+++ ex9.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Exercise 9: Printing, Printing, Printing
+
+# Here's some new strange stuff, remember to type it exaclty.
+
+# set days to a long string of weekday names
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# save all names of the months seperated by new line to the variable
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+# print the string days. There will be two whitespace between text & variable
+print("Here are the days: ", days)
+# print the string months. There will be two whitespace between text & variable
+print("Here are the months: ", months)
+
+# Print a multiline text
+print("""
+There's something goind on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
