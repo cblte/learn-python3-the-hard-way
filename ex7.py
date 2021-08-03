7df9fd289ec2785d80e900e9bfe3c514b942534a
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-08-03 16:42:52 +0200

    Add exercise 7 More printing

---
+++ ex7.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Exercise 7: More printing
+
+# print out a string
+print("Mary had a little lamb.")
+# print out the formatted string
+print("Its fleece was white as {}.".format('snow'))
+# print out a string
+print("And everywhere that Mary went.")
+# print out the dot ten times
+print("." * 10)
+
+# define 12 variables each containing on letter only
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Concatenate the first 6 variables and print them followed
+# by a whitespace at the end
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# Concatenate variable 7 to 12 and print out the string
+print(end7 + end8 + end9 + end10 + end11 + end12)
