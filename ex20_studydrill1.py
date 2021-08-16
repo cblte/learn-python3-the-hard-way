e160fc01d64d91adf27792d42ce35122ff696791
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-08-16 12:15:34 +0200

    Add exercise

--- ex20_studydrill1.py
+++ ex20_studydrill1.py
@@ -2,19 +2,27 @@
 
 # Exercise 20. Functions and Files
 
+# import the argv modules of sys package
 from sys import argv
 
+# unpacka arguments
 script, input_file = argv
 
 
+# define a function that takes a file handle and prints the contents
+# of the whole file
 def print_all(file):
     print(file.read())
 
 
+# define a function to set the read cursor back
+# for the passed file referene.
 def rewind(file):
     file.seek(0)
 
 
+# defina a functoin that only reads one line at a time
+# this line is then printed to stdout
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
