39ada2b2a21c05bc0bf7a37b672670ef2f95dfba
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-09-12 22:14:44 +0200

    Add exercise

---
+++ ex33_sd1.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+# Exercise 33. While Loops
+# Study Drill 1/2
+
+
+def number_loop(max, incr = 1):
+    """
+    use a while loop up to number max, use incr to set the increment
+    """
+
+    i = 0
+    numbers = []
+
+    while i < max:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i = i + incr
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+
+    print("The Numbers")
+    for num in numbers:
+        print(num)
+
+
+if __name__ == "__main__":
+    number_loop(8,2)
---
+++ ex33_sd3.py
@@ -0,0 +1,28 @@
+#!/bin/env python3
+
+# Exercise 33. While Loops
+# Study Drill 3
+
+
+def number_loop(max, incr=1):
+    """
+    use a while loop up to number max, use incr to set the increment
+    """
+
+    i = 0
+    numbers = []
+
+    for i in range(0, max, incr):
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+
+    print("The Numbers")
+    for num in numbers:
+        print(num)
+
+
+if __name__ == "__main__":
+    number_loop(100, 3)
