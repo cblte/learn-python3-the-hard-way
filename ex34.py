42c9fd88e2cb7e4899476bafdf89dabde7f6fcf8
Carsten Brueggenolte <mail@cbrueggenolte.de>
2021-09-13 23:27:19 +0200

    Add exercise

---
+++ ex34.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Exercise 34. Accessing Elements of Lists
+
+animals = ['bear','python3.6','peacock','kangaroo','whale','platypus']
+
+# The animal at 1 is python3.6
+print(animals[1])
+# The third animal is at 2 and is a peacock
+print(animals[2])
+# The first animal is at 0 and is a bear
+print(animals[0])
+# The animal at 3 is kangaroo
+print(animals[3])
+# The fifth animal is at 4 and is a whale
+print(animals[4])
+# The animal at 2 is a peacock
+print(animals[2])
+# The sixth animal is at 5 and is a platypus
+print(animals[5])
+# The animal at 4 is a whale
+print(animals[4])
